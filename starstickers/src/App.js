0d0f5a48393978dfc17ee9a4550308430acae951
cbituin02 <97369678+cbituin02@users.noreply.github.com>
2022-03-09 19:41:40 -0800

    Session1

---
+++ starstickers/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from  './components/navBar';
+import Footer from './components/footer';
+
+function App() {
+  return ( <div className="App">
+            <NavBar></NavBar>
+            <h1>Hello World!</h1>
+            <footer>Copyright by: Caleb Bituin</footer>
+          </div> );
+}
+
+export default App;
---
+++ starstickers/src/components/navBar.jsx
@@ -0,0 +1,9 @@
+import './navBar.css';
+
+const NavBar = () => {
+    return (<div className='navBar'>
+                <h2>This will be the navBar</h2>
+            </div>);  
+};
+
+export default NavBar;
